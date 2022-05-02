249fe581bf895e779625d5f16fea90f9d5c53b35
Peyton <pekellogg@hotmail.com>
2022-05-02 14:52:19 -0700

    passing testing in spec/features/student_spec.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :students, only: :index
+
+  # below assumes 7 RESTful routes
+  # get 'posts/:id', to: 'posts#show'
+
+  # below assumes only the index/7 total
+  get 'students/:id', to: 'students#show'
 end
